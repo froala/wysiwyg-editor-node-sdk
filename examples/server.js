391e1c78efdc82a83873f23f64d610b9b4e75992
Popescu <florin.popescu0110@gmail.com>
2016-07-21 00:45:02 +0300

    added file upload + generic upload.

--- examples/server.js
+++ examples/server.js
@@ -1,19 +1,32 @@
 var express = require('express');
 var app = express();
 var cors = require('cors')
-var FroalaEditor = require('../index.js');
+var FroalaEditor = require('../lib/editor.js');
 
 app.use(cors());
 
 app.post('/upload_image', function (req, res) {
 
-	FroalaEditor.uploadImage(req, res);
-	// or call FroalaEditor.uploadImage(req, res, options);
+  FroalaEditor.uploadImage(req, res);
+  // or call FroalaEditor.uploadImage(req, res, options);
+});
+
+app.post('/upload_file', function (req, res) {
+
+  FroalaEditor.uploadFile(req, res);
+  // or call FroalaEditor.uploadFile(req, res, options);
+});
+
+// generic upload
+app.post('/upload', function (req, res) {
+
+  FroalaEditor.upload(req, res);
+  // or call FroalaEditor.upload(req, res, options);
 });
 
 app.get('/uploads/:filename', function (req, res) {
 
-	res.sendFile(path.join(__dirname, req.params.filename));
+  res.sendFile(path.join(__dirname, req.params.filename));
 });
 
 app.listen(3000, function () {
