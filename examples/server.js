77e111c896d34e8ac3d9a97b5fdbc1d8e4718dd9
DhivaharM <dhivahar.m@celestialsys.com>
2026-05-13 21:32:38 +0530

    logic for Froala-724

--- examples/server.js
+++ examples/server.js
@@ -7,10 +7,25 @@ var fs = require('fs');
 var gm = require('gm').subClass({imageMagick: true});
 var FroalaEditor = require('../lib/froalaEditor.js');
 var Collaborative = FroalaEditor.Collaborative;
+var CollabPersistence = FroalaEditor.CollabPersistence;
+
+// Permissive CORS for the dev environment so the editor's webpack dev server
+// (port 8001) can hit the SDK's REST endpoints (port 3000).
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
 
 app.use(express.static(__dirname + '/'));
 app.use('/bower_components',  express.static(path.join(__dirname, '../bower_components')));
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Attach suggestion + comment persistence routes.
+CollabPersistence.attachRoutes(app, { dbPath: path.join(__dirname, '..', 'collab.db') });
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
